e3884727c38bb8231ef54008603513004b52b6c1
Oleh Dubishchev <Oleh.Dubishchev@infopulse.com>
2022-05-14 13:41:38 +0300

    task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Data on air temperature for ten days of November are stored in an array. Determine how many times the temperature dropped below -10 degrees.
+
+            var tempretureArray = new int[10];
+            for (int i = 0; i < tempretureArray.Length; i++)
+            {
+                Random rnd = new Random();
+                tempretureArray[i] = rnd.Next(-50,50);
+            }
+
+            var temperatureDroppedBelowCounter = 0;
+            foreach (var item in tempretureArray)
+            {
+                if (item <= -10)
+                    temperatureDroppedBelowCounter++;
+            }
+
+            foreach (var item in tempretureArray)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Numbers of days below -10 degrees is {temperatureDroppedBelowCounter}");
         }
     }
 }
