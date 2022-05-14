0f9a9b3b3e2d410a7f5ec7a146e44aa7696ab85b
Oleh Dubishchev <Oleh.Dubishchev@infopulse.com>
2022-05-14 13:10:13 +0300

    add task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Write a console program where the user enters a number from the keyboard.
+            //If the number is either 5 or 10, then the program outputs "The number is either 5 or 10". Otherwise, the program displays the message "Unknown number".
+            
+            Console.WriteLine("Enter number:");
+            var inputNumber = int.Parse(Console.ReadLine());
+            if (inputNumber == 5 || inputNumber == 10)
+                Console.WriteLine("The number is either 5 or 10");
+            Console.WriteLine("Unknown number");
         }
     }
 }
